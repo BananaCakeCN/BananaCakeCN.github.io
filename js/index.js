e07e2d2ee0352f02d150b3cd025414e52d4433dd
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2022-04-30 07:01:26 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-
+if (document.referrer=='https://t.co/'){
+    if (document.cookie!='from=other'){
+        document.cookie = "from=twitter; expires=" + new Date(new Date().setTime(new Date().getTime() + 730*86400000)).toUTCString() + "; path=/";
+    }
+}else{
+    document.cookie = "from=other; expires=" + new Date(new Date().setTime(new Date().getTime() + 730*86400000)).toUTCString() + "; path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+         }
+     }
+    return "";
+} 
+console.log(getCookie('from'))
